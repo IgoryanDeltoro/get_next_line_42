b928ecd258bbb4997d4f4cd415531837e2ab62dd
IgoryanDeltoro <ibondarc@c2r3p9.42vienna.com>
2024-10-09 14:45:24 +0200

    added bonus part and refactored mandatory  getnextlin

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibondarc <ibondarc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 12:57:05 by ibondarc          #+#    #+#             */
+/*   Updated: 2024/10/09 14:35:12 by ibondarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# define OPEN_FILE 1024
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlcat(char *dst, char *src, size_t size);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibondarc <ibondarc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 11:17:00 by ibondarc          #+#    #+#             */
+/*   Updated: 2024/10/09 14:35:24 by ibondarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	total_len;
+
+	if (!s1 || !s2)
+		return (0);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	total_len = s1_len + s2_len;
+	result = malloc(sizeof(char) * (total_len + 1));
+	if (!result)
+		return (NULL);
+	result[total_len] = '\0';
+	while (s1_len--)
+	{
+		result[s1_len] = s1[s1_len];
+	}
+	while (total_len-- > ft_strlen(s1))
+	{
+		result[total_len] = s2[--s2_len];
+	}
+	return (result);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*result;
+	int		s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	result = malloc(sizeof(char) * (s_len + 1));
+	if (!result)
+		return (NULL);
+	result[s_len] = '\0';
+	while (s_len--)
+		result[s_len] = s[s_len];
+	return (result);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
